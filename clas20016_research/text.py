3e0c8063fd7783fd048d4c2345fefcfee9ecefa0
jwillmur <88318566+jwillmur@users.noreply.github.com>
2022-10-08 23:57:05 +1100

    book1 and 2 instances

---
+++ clas20016_research/text.py
@@ -0,0 +1,128 @@
+ship = []
+epithet = []
+number = []
+case = []
+clause = []
+scansion = []
+
+## BOOK ONE
+## line 211
+ship.append('νηυσίν')
+epithet.append(['κοίληις', 'hollow'])
+number.append('s')
+case.append('dat')
+clause.append('πρίν γε τὸν ἐς Τροίην ἀναβήμεναι, ἔνθά περ ἄλλοι | Ἀργείων οἱ ἄριστοι ἔβαν κοίληις ἐνὶ νηυσίν·')
+scansion.append('-- -uu -uu -- -uu -uu') # not sure
+
+## line 260
+ship.append('νηὸς')
+epithet.append(['θοῆς', 'swift'])
+number.append('s')
+case.append('gen')
+clause.append('ὤιχετο γὰρ καὶ κεῖσε θοῆς ἐπὶ νηὸς Ὀδυσσεύς | φάρμακον ἀνδροφόνον διζήμενος,')
+scansion.append('-uu -- -uu -uu -uu --')
+
+## line 303
+ship.append('νῆα')
+epithet.append(['θοὴν', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('αὐτὰρ ἐγὼν ἐπὶ νῆα θοὴν κατελεύσομαι ἤδη | ἠδ’ ἑτάρους, οἵ πού με μάλ’ ἀσχαλόωσι μένοντες·')
+scansion.append('-uu -uu -uu -uu -uu --')
+
+## BOOK TWO
+## line 18
+ship.append('νηυσίν')
+epithet.append(['κοίληις', 'hollow'])
+number.append('s')
+case.append('dat')
+clause.append('καὶ γὰρ τοῦ φίλος υἱὸς ἅμ’ ἀντιθέωι Ὀδυσῆϊ | Ἴλιον εἲς εὔπωλον ἔβη κοίληις ἐνὶ νηυσίν,')
+scansion.append('-uu -- -uu -- -uu -uu')
+
+## line 27
+ship.append('νηυσίν')
+epithet.append(['κοίληις', 'hollow'])
+number.append('s')
+case.append('dat')
+clause.append('οὔτέ ποθ’ ἡμετέρη ἀγορὴ γένετ’ οὔτε θόωκος | ἐξ οὗ Ὀδυσσεὺς δῖος ἔβη κοίληις ἐνὶ νηυσίν.')
+scansion.append('-uu -- -uu -- -uu -uu')
+
+## line 212
+ship.append('νῆα')
+epithet.append(['θοὴν', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('ἀλλ’ ἄγε μοι δότε νῆα θοὴν καὶ εἴκοσ’ ἑταίρους,')
+scansion.append('-uu -uu -uu -- -uu --')
+
+## line 287
+ship.append('νῆα')
+epithet.append(['θοὴν', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('ὅς τοι νῆα θοὴν στελέω καὶ ἅμ’ ἕψομαι αὐτός')
+scansion.append('-- -uu -uu -uu -uu --')
+
+## line 332
+ship.append('νηός')
+epithet.append(['κοίλης', 'hollow'])
+number.append('s')
+case.append('gen')
+clause.append('“τίς δ’ οἶδ’, εἴ κε καὶ αὐτὸς ἰὼν κοίλης ἐπὶ νηός | τῆλε φίλων ἀπόληται ἀλώμενος')
+scansion.append('-- -uu -uu -- -uu --')
+
+## line 385
+ship.append('νῆα')
+epithet.append(['θοὴν', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('ἑσπερίους δ’ ἐπὶ νῆα θοὴν ἀγέρεσθαι ἀνώγει.')
+scansion.append('-uu -uu -uu -uu -uu --')
+
+## line 387
+ship.append('νῆα')
+epithet.append(['θοήν·', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('ἣ δ’ αὖτε Φρονίοιο Νοήμονα φαίδιμον υἱόν | ἤιτεε νῆα θοήν·')
+scansion.append('-uu -uu -uu -- -uu --')
+
+## line 389
+ship.append('νῆα')
+epithet.append(['θοὴν', 'swift'])
+number.append('s')
+case.append('acc')
+clause.append('καὶ τότε νῆα θοὴν ἅλαδ’ εἴρυσε,')
+scansion.append('-uu -uu -uu -uu -uu --')
+
+## line 390
+ship.append('νῆες')
+epithet.append(['ἐΰσσελμοι', 'well-benched'])
+number.append('p')
+case.append('nom')
+clause.append('τά τε νῆες ἐΰσσελμοι φορέουσιν.')
+scansion.append('-uu -uu -uu -- -uu --') # unsure
+
+## line 414
+ship.append('νηΐ')
+epithet.append(['ἐϋσσέλμωι', 'well-benched'])
+number.append('s')
+case.append('dat')
+clause.append('οἳ δ’ ἄρα πάντα φέροντες ἐϋσσέλμωι ἐνὶ νηΐ | κάτθεσαν,')
+scansion.append('-uu -uu -uu -- -uu --')
+
+## line 
+ship.append('νῆα')
+epithet.append(['θοὴν μέλαιναν', 'swift black'])
+number.append('s')
+case.append('acc')
+clause.append('δησάμενοι δ’ ἄρα ὅπλα θοὴν ἀνὰ νῆα μέλαιναν | στήσαντο κρητῆρας ἐπιστεφέας οἴνοιο,')
+scansion.append('')
+
+## line 
+ship.append('')
+epithet.append(['', ''])
+number.append('')
+case.append('')
+clause.append('')
+scansion.append('')
